3ec4b58f72fb1ba850f2eada56f21e44bee18308
fernandoavilesrosado <fernandoavilesrosado@gmail.com>
2021-09-23 15:30:15 +0000

    add proyect

--- src/models.py
+++ src/models.py
@@ -1,19 +1,100 @@
-from flask_sqlalchemy import SQLAlchemy
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String, Date
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+from eralchemy import render_er
 
-db = SQLAlchemy()
+Base = declarative_base()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+class User(Base):
+    __tablename__ = 'user'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    lastname = Column(String(200), nullable=False)
+    _password = Column(String(150), nullable=False)
+    _mickname = Column(String(150), nullable = False)
+    email = Column(String(150), nullable = False)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+    id_user_fav = Column(Integer, ForeignKey('user.id'))
+    user_ = relationship ('User')
+
+    id_people_fav = Column(Integer, ForeignKey('people_view.id'))
+    people = relationship('People_view')
+
+    #id_planet_fav  = Column(Integer, ForeignKey('planet_view.id'))
+    #id_planet_= relationship('Planet_view')
+    
+    id_starship_fav = Column(Integer, ForeignKey('starship_view.id'))
+    id_starship = relationship('Starship_view')
+
+
+
+
+class People_view(Base):
+    __tablename__='people_view'
+    id = Column (Integer, primary_key = True)
+    url = Column (String, nullable= True )  
+    name = Column(String(150), nullable = True)
+    height = Column(Integer, nullable= False)
+    mass = Column(Integer, nullable=False)
+    hair_color = Column(String(50), nullable=False)
+    skin_color = Column(String(50), nullable = False)
+    eyes_color = Column(String(50), nullable = False)
+    birth_year = Column(Date,nullable = False)
+    created  = Column(String, nullable = False)
+    edited = Column(String, nullable = False)
+    homeworld = Column(String, nullable = True)
+
+class Starship_view(Base):
+    __tablename__='starship_view'
+    id = Column(Integer, primary_key = True)
+    url = Column(String, nullable=False)
+    name = Column(String, nullable = True )
+    model = Column(String, nullable = False)
+    straship_ = Column(String, nullable = False)
+    manufacture = Column(String, nullable = False)
+    incredit = Column(String, nullable = False )
+    lenght = Column(String, nullable = False)
+    grew = Column(String, nullable = False)
+    passenger = Column(String, nullable = False)
+    max_atmospheric_speed = Column(String, nullable = False)
+    hyperdrive_rating = Column(String, nullable = False)
+    mglt = Column(String, nullable = False)
+    cargo_capacity = Column(String, nullable = False)
+    consumables = Column(String, nullable = False)
+    pilot = Column(String, nullable = False)
+    edited = Column(String, nullable = False)
+    created = Column(String, nullable = False)
+
+class Planet_view(Base):
+    __tablename__='planet_view'
+    id = Column(Integer, primary_key = True)
+    url = Column(String, nullable = True )
+    name =  Column(String, nullable = True )
+    diameter = Column(Integer, nullable = False)
+    rotation_period = Column(String, nullable = False)
+    orbital_period = Column(String, nullable = False)
+    gravity = Column(String, nullable = False)
+    population = Column(String, nullable = False)
+    climate = Column(String, nullable = False)
+    terrain = Column(String, nullable = False)
+    surface_water = Column(String, nullable = False)
+    create = Column(String, nullable = False)
+    edited = Column(String, nullable = False)
+    description = Column(String, nullable = False)
+
+
+
+   
+## Draw from SQLAlchemy base
+render_er(Base, 'diagram.png')
