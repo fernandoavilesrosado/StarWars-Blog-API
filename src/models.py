aeb4a6adefe47d8efb3b4360ccdc491b16db1543
fernandoavilesrosado <fernandoavilesrosado@gmail.com>
2021-09-23 16:49:50 +0000

    add DB

--- src/models.py
+++ src/models.py
@@ -10,91 +10,81 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'user'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(100), nullable=False)
-    lastname = Column(String(200), nullable=False)
-    _password = Column(String(150), nullable=False)
-    _mickname = Column(String(150), nullable = False)
-    email = Column(String(150), nullable = False)
+    id = db.Column(db.Integer, primary_key=True)
+    _password = db.Column(db.String(150), nullable=False)
+    _nickname = db.Column(db.String(150), nullable = False)
+    name = db.Column(db.String(100), nullable=False)
+    lastname = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(150), nullable = False)
+
+    have_favorites = relationship("Favorites", backref="user")
 
 class Favorites(Base):
     __tablename__ = 'favorites'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, ForeignKey('user.id'))
+    people_id = db.Column(db.Integer, ForeignKey('people.id'))
+    starship_id = db.Colum(db.Integer, ForeignKey("starship.id"))
+    planet_id = db.Colum(db.Integer, ForeignKey("planet.id"))
 
-    id_user_fav = Column(Integer, ForeignKey('user.id'))
-    user_ = relationship ('User')
+class People(Base):
+    __tablename__='people'
+    id = db.Column (db.Integer, primary_key = True)
+    url = db.Column (db.String, nullable= True )  
+    name = db.Column(db.String(150), nullable = True)
+    height = db.Column(db.Integer, nullable= False)
+    mass = db.Column(db.Integer, nullable=False)
+    hair_color = db.Column(db.String(50), nullable=False)
+    skin_color = db.Column(db.String(50), nullable = False)
+    eyes_color = db.Column(db.String(50), nullable = False)
+    birth_year = db.Column(db.Date,nullable = False)
+    created  = db.Column(db.String, nullable = False)
+    edited = db.Column(db.String, nullable = False)
+    homeworld = db.Column(db.String, nullable = True)
 
-    id_people_fav = Column(Integer, ForeignKey('people_view.id'))
-    people = relationship('People_view')
+    have_favorites = relationship("Favorites", backref="people")
 
-    #id_planet_fav  = Column(Integer, ForeignKey('planet_view.id'))
-    #id_planet_= relationship('Planet_view')
-    
-    id_starship_fav = Column(Integer, ForeignKey('starship_view.id'))
-    id_starship = relationship('Starship_view')
+class Starship(Base):
+    __tablename__='starship'
+    id = db.Column(db.Integer, primary_key = True)
+    url = db.Column(db.String, nullable=False)
+    name = db.Column(db.String, nullable = True )
+    model = db.Column(db.String, nullable = False)
+    straship_ = db.Column(db.String, nullable = False)
+    manufacture = db.Column(db.String, nullable = False)
+    incredit = db.Column(db.String, nullable = False )
+    lenght = db.Column(db.String, nullable = False)
+    grew = db.Column(db.String, nullable = False)
+    passenger = db.Column(db.String, nullable = False)
+    max_atmospheric_speed = db.Column(db.String, nullable = False)
+    hyperdrive_rating = db.Column(db.String, nullable = False)
+    mglt = db.Column(db.String, nullable = False)
+    cargo_capacity = db.Column(db.String, nullable = False)
+    consumables = db.Column(db.String, nullable = False)
+    pilot = db.Column(db.String, nullable = False)
+    edited = db.Column(db.String, nullable = False)
+    created = db.Column(db.String, nullable = False)
 
+    have_favorites = relationship("Favorites", backref="starhip")
 
+class Planet(Base):
+    __tablename__='planet'
+    id = db.Column(db.Integer, primary_key = True)
+    url = db.Column(db.String, nullable = True )
+    name =  db.Column(db.String, nullable = True )
+    diameter = db.Column(db.Integer, nullable = False)
+    rotation_period = db.Column(db.String, nullable = False)
+    orbital_period = db.Column(db.String, nullable = False)
+    gravity = db.Column(db.String, nullable = False)
+    population = db.Column(db.String, nullable = False)
+    climate = db.Column(db.String, nullable = False)
+    terrain = db.Column(db.String, nullable = False)
+    surface_water = db.Column(db.String, nullable = False)
+    create = db.Column(db.String, nullable = False)
+    edited = db.Column(db.String, nullable = False)
+    description = db.Column(db.String, nullable = False)
 
+    have_favorites = relationship("Favorites", backref="planet")
 
-class People_view(Base):
-    __tablename__='people_view'
-    id = Column (Integer, primary_key = True)
-    url = Column (String, nullable= True )  
-    name = Column(String(150), nullable = True)
-    height = Column(Integer, nullable= False)
-    mass = Column(Integer, nullable=False)
-    hair_color = Column(String(50), nullable=False)
-    skin_color = Column(String(50), nullable = False)
-    eyes_color = Column(String(50), nullable = False)
-    birth_year = Column(Date,nullable = False)
-    created  = Column(String, nullable = False)
-    edited = Column(String, nullable = False)
-    homeworld = Column(String, nullable = True)
-
-class Starship_view(Base):
-    __tablename__='starship_view'
-    id = Column(Integer, primary_key = True)
-    url = Column(String, nullable=False)
-    name = Column(String, nullable = True )
-    model = Column(String, nullable = False)
-    straship_ = Column(String, nullable = False)
-    manufacture = Column(String, nullable = False)
-    incredit = Column(String, nullable = False )
-    lenght = Column(String, nullable = False)
-    grew = Column(String, nullable = False)
-    passenger = Column(String, nullable = False)
-    max_atmospheric_speed = Column(String, nullable = False)
-    hyperdrive_rating = Column(String, nullable = False)
-    mglt = Column(String, nullable = False)
-    cargo_capacity = Column(String, nullable = False)
-    consumables = Column(String, nullable = False)
-    pilot = Column(String, nullable = False)
-    edited = Column(String, nullable = False)
-    created = Column(String, nullable = False)
-
-class Planet_view(Base):
-    __tablename__='planet_view'
-    id = Column(Integer, primary_key = True)
-    url = Column(String, nullable = True )
-    name =  Column(String, nullable = True )
-    diameter = Column(Integer, nullable = False)
-    rotation_period = Column(String, nullable = False)
-    orbital_period = Column(String, nullable = False)
-    gravity = Column(String, nullable = False)
-    population = Column(String, nullable = False)
-    climate = Column(String, nullable = False)
-    terrain = Column(String, nullable = False)
-    surface_water = Column(String, nullable = False)
-    create = Column(String, nullable = False)
-    edited = Column(String, nullable = False)
-    description = Column(String, nullable = False)
-
-
-
-   
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
